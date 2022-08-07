10d7dd95e78ddba8db0b990053d82204de3bb1af
Dineshraj <dineshrajanandan@gmail.com>
2022-08-07 21:06:12 +0530

    separating project data from html

---
+++ data.js
@@ -0,0 +1,212 @@
+data = [
+  {
+    "title": "Stats preview card component",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62",
+    "siteLink": "./newbie/stats-preview-card-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/stats-preview-card-component"
+  },
+  {
+    "title": "3-column preview card component",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-",
+    "siteLink": "./newbie/3-column-preview-card-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/3-column-preview-card-component"
+  },
+  {
+    "title": "Profile card component",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/profile-card-component-cfArpWshJ",
+    "siteLink": "./newbie/profile-card-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/profile-card-component"
+  },
+  {
+    "title": "FAQ accordion card",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam",
+    "siteLink": "./newbie/faq-accordion-card/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/faq-accordion-card"
+  },
+  {
+    "title": "Social proof section",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/social-proof-section-6e0qTv_bA",
+    "siteLink": "./newbie/social-proof-section/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/social-proof-section"
+  },
+  {
+    "title": "Article preview component",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT",
+    "siteLink": "./newbie/article-preview-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/article-preview-component"
+  },
+  {
+    "title": "Four card feature section",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK",
+    "siteLink": "./newbie/four-card-feature-section/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/four-card-feature-section"
+  },
+  {
+    "title": "Base Apparel coming soon",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0",
+    "siteLink": "./newbie/base-apparel-coming-soon/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/base-apparel-coming-soon"
+  },
+  {
+    "title": "Intro component with sign up form",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1",
+    "siteLink": "./newbie/intro-component-with-signup-form/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/intro-component-with-signup-form"
+  },
+  {
+    "title": "Single price grid component",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc",
+    "siteLink": "./newbie/single-price-grid-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/single-price-grid-component"
+  },
+  {
+    "title": "Ping coming soon page",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da",
+    "siteLink": "./newbie/ping-coming-soon-page/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/ping-coming-soon-page"
+  },
+  {
+    "title": "Huddle landing page with single introductory section",
+    "level": "newbie",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0",
+    "siteLink": "./newbie/huddle-landing-page-with-single-introductory-section/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/newbie/huddle-landing-page-with-single-introductory-section"
+  },
+  {
+    "title": "Sunnyside agency landing page",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/sunnyside-agency-landing-page-7yVs3B6ef",
+    "siteLink": "./junior/sunnyside-agency-landing-page/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/sunnyside-agency-landing-page"
+  },
+  {
+    "title": "Blogr landing page",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/blogr-landing-page-EX2RLAApP",
+    "siteLink": "./junior/blogr-landing-page-main/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/blogr-landing-page-main"
+  },
+  {
+    "title": "Crowdfunding product page",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/crowdfunding-product-page-7uvcZe7ZR",
+    "siteLink": "./junior/crowdfunding-product-page/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/crowdfunding-product-page"
+  },
+  {
+    "title": "Interactive pricing component",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8",
+    "siteLink": "./junior/interactive-pricing-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/interactive-pricing-component"
+  },
+  {
+    "title": "Loopstudios landing page",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw",
+    "siteLink": "./junior/loopstudios-landing-page/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/loopstudios-landing-page"
+  },
+  {
+    "title": "Testimonials grid section",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/testimonials-grid-section-Nnw6J7Un7",
+    "siteLink": "./junior/testimonials-grid-section/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/testimonials-grid-section"
+  },
+  {
+    "title": "Fylo data storage component",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/fylo-data-storage-component-1dZPRbV5n",
+    "siteLink": "./junior/fylo-data-storage-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/fylo-data-storage-component"
+  },
+  {
+    "title": "Coding bootcamp testimonials slider",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL",
+    "siteLink": "./junior/coding-bootcamp-testimonials-slider/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/coding-bootcamp-testimonials-slider"
+  },
+  {
+    "title": "Social media dashboard with theme switcher",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H",
+    "siteLink": "./junior/social-media-dashboard-with-theme-switcher/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/social-media-dashboard-with-theme-switcher"
+  },
+  {
+    "title": "Tip calculator app",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX",
+    "siteLink": "./junior/tip-calculator-app-main/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/tip-calculator-app-main"
+  },
+  {
+    "title": "Pricing component with toggle",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC",
+    "siteLink": "./junior/pricing-component-with-toggle/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/pricing-component-with-toggle"
+  },
+  {
+    "title": "Insure landing page",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/insure-landing-page-uTU68JV8",
+    "siteLink": "./junior/insure-landing-page/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/insure-landing-page"
+  },
+  {
+    "title": "Project tracking intro component",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/project-tracking-intro-component-5d289097500fcb331a67d80e",
+    "siteLink": "./junior/project-tracking-intro-component/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/project-tracking-intro-component"
+  },
+  {
+    "title": "Clipboard landing page",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/clipboard-landing-page-5cc9bccd6c4c91111378ecb9",
+    "siteLink": "./junior/clipboard-landing-page/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/clipboard-landing-page"
+  },
+  {
+    "title": "Huddle landing page with alternating feature blocks",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/huddle-landing-page-with-alternating-feature-blocks-5ca5f5981e82137ec91a5100",
+    "siteLink": "./junior/huddle-landing-page-with-alternating-feature-blocks/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/huddle-landing-page-with-alternating-feature-blocks"
+  },
+  {
+    "title": "Fylo dark theme landing page",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/fylo-dark-theme-landing-page-5ca5f2d21e82137ec91a50fd",
+    "siteLink": "./junior/fylo-dark-theme-landing-page/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/fylo-dark-theme-landing-page"
+  },
+  {
+    "title": "Huddle landing page with curved sections",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/huddle-landing-page-with-curved-sections-5ca5ecd01e82137ec91a50f2",
+    "siteLink": "./junior/huddle-landing-page-with-curved-sections/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/huddle-landing-page-with-curved-sections"
+  },
+  {
+    "title": "Time tracking dashboard",
+    "level": "junior",
+    "frontEndMentorLink": "https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw",
+    "siteLink": "./junior/time-tracking-dashboard/index.html",
+    "codeLink": "https://github.com/DineshrajAnandan/FrontEndMentorChallenges/tree/main/junior/time-tracking-dashboard"
+  }
+]
--- main.js
+++ main.js
@@ -1,46 +1,87 @@
 document.onreadystatechange = () => {
-    if(document.readyState == 'complete') {
-        registerEvents();
-    }
-}
+  if (document.readyState == "complete") {
+    generateProjectGrid();
+    registerEvents();
+  }
+};
 
 function registerEvents() {
-    document.querySelector('.filter-item.all').addEventListener('click',filterAllItems);
-    document.querySelector('.filter-item.newbie').addEventListener('click',filterLevel1Items);
-    document.querySelector('.filter-item.junior').addEventListener('click',filterLevel2Items);
+  document
+    .querySelector(".filter-item.all")
+    .addEventListener("click", filterAllItems);
+  document
+    .querySelector(".filter-item.newbie")
+    .addEventListener("click", filterLevel1Items);
+  document
+    .querySelector(".filter-item.junior")
+    .addEventListener("click", filterLevel2Items);
 }
 
 function filterAllItems(el) {
-    clearAllFilters();
-    el.currentTarget.classList.add('filter-selected');
-    document.querySelectorAll('.portfolio-item').forEach ( el => {
-        el.style.display = 'block';
-    })
+  clearAllFilters();
+  el.currentTarget.classList.add("filter-selected");
+  document.querySelectorAll(".portfolio-item").forEach((el) => {
+    el.style.display = "block";
+  });
 }
 
 function filterLevel1Items(el) {
-    clearAllFilters();
-    el.currentTarget.classList.add('filter-selected');
-    document.querySelectorAll('.portfolio-item.newbie').forEach ( el => {
-        el.style.display = 'block';
-    })
+  clearAllFilters();
+  el.currentTarget.classList.add("filter-selected");
+  document.querySelectorAll(".portfolio-item.newbie").forEach((el) => {
+    el.style.display = "block";
+  });
 }
 
 function filterLevel2Items(el) {
-    clearAllFilters();
-    el.currentTarget.classList.add('filter-selected');
-    document.querySelectorAll('.portfolio-item.junior').forEach ( el => {
-        el.style.display = 'block';
-    })
+  clearAllFilters();
+  el.currentTarget.classList.add("filter-selected");
+  document.querySelectorAll(".portfolio-item.junior").forEach((el) => {
+    el.style.display = "block";
+  });
 }
 
 function clearAllFilters() {
-    document.querySelectorAll('.filter-item').forEach ( el => {
-        el.classList.remove('filter-selected');
-    })
+  document.querySelectorAll(".filter-item").forEach((el) => {
+    el.classList.remove("filter-selected");
+  });
 
-    document.querySelectorAll('.portfolio-item').forEach ( el => {
-        el.style.display = 'none';
-    })
+  document.querySelectorAll(".portfolio-item").forEach((el) => {
+    el.style.display = "none";
+  });
+}
 
+function generateProjectGrid() {
+  const projectData = JSON.parse(JSON.stringify(data));
+  let projectGridView = "";
+  projectData.forEach((item) => {
+    projectGridView += getGridItemHtmlText(item);
+  });
+
+  let container = document.querySelector("div.portfolio-container");
+  container.innerHTML = projectGridView;
+}
+
+function getGridItemHtmlText(itemData) {
+  return `
+    <div class="portfolio-item ${itemData.level}">
+        <div class="title">${itemData.title}</div>
+        <div class="challenge-flex">
+            <div class="item-level ${itemData.level}"></div>
+            <div class="view-challenge-button">
+                <a href="${itemData.frontEndMentorLink}" target="blank">
+                    View challenge
+                </a>
+            </div>
+        </div>
+        <a href="${itemData.siteLink}">
+            <div class="view-sol-button"> <i class="fas fa-link"></i> View solution</div>
+        </a>
+        <div class="view-code">
+            <a href="${itemData.codeLink}" target="blank">
+                <i class="fab fa-github fa-lg"></i>
+            </a>
+        </div>
+    </div>
+    `;
 }
